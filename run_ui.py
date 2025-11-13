9c870f884a28eaf8b0202ffaa5075e1aa52238d9
Harikrushna_Goti <167052236+Harikrushna2272@users.noreply.github.com>
2025-11-13 13:06:05 +0530

    Update run_ui.py

--- run_ui.py
+++ run_ui.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Script to run the Streamlit UI."""
 
-import streamlit.cli as stcli
+from streamlit.web import cli as stcli
 import os
 import sys
 
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     # Get the absolute path to the app.py file
     current_dir = os.path.dirname(os.path.abspath(__file__))
     app_path = os.path.join(current_dir, "annual_report_analysis", "ui", "app.py")
-    
+
     sys.argv = ["streamlit", "run", app_path]
     stcli.main()
